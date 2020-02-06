254cfd1ccf09f68cf6e81d7b087a0d3db9511feb
Kummita Sriteja <sriteja.ku@gmail.com>
2020-02-06 10:38:26 +0100

    Updating dynamicCG to run test cases from a file passed as an argument

--- dynamicCG.py
+++ dynamicCG.py
@@ -1,6 +1,15 @@
+import sys
 import pytest
 import py_call_graph
 
 
 if __name__ == '__main__':
-    pytest.main(['-x', './tests/'], plugins=[py_call_graph])
+    if len(sys.argv) > 1:
+        tc_file = sys.argv[1]
+        tc_to_run = ['-x']
+        with open(tc_file) as f:
+            for line in f:
+                tc_to_run.append(str(line.replace("\n", "")))
+        pytest.main(tc_to_run, plugins=[py_call_graph])
+    else:
+        pytest.main(['-x', './tests/'], plugins=[py_call_graph])

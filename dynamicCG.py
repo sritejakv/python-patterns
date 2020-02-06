799339fe46c997fc699ebb2f1d917709bddecfa0
Kummita Sriteja <sriteja.ku@gmail.com>
2020-02-06 12:28:07 +0100

    Update dynamicCG.py
    - Running each test case seperately in order to not stop on exceptions.

--- dynamicCG.py
+++ dynamicCG.py
@@ -6,10 +6,8 @@ import py_call_graph
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         tc_file = sys.argv[1]
-        tc_to_run = ['-x']
         with open(tc_file) as f:
             for line in f:
-                tc_to_run.append(str(line.replace("\n", "")))
-        pytest.main(tc_to_run, plugins=[py_call_graph])
+                pytest.main(['-x', str(line.replace("\n", ""))], plugins=[py_call_graph])
     else:
         pytest.main(['-x', './tests/'], plugins=[py_call_graph])
